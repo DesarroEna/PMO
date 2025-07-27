f13e2a2a7db4dfeac7e1cd447c40e18a7f6915c7
prodriguez25 <pablo25g.apr@gmail.com>
2025-07-27 20:32:30 -0300

    Completo homework de generador de contraseñas

--- 02. JS Bases/generadorContrasenas.js
+++ 02. JS Bases/generadorContrasenas.js
@@ -1,40 +1,49 @@
 function checkLongitud(longitud) {
   /* TU CODIGO */
-
+  if (longitud >= 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function generarContrasena(longitud, incluirEspeciales, incluirNumeros, incluirMayusculas ) {
   /* TU CODIGO */
-  var letras = null;
+  var letras = 'abcdefghijklmnopqrstuvwxyz';
 
-  var numeros = null;
+  var numeros = '0123456789';
 
-  var especiales = null;
+  var especiales = '!@#$%^&*()_+-=[]{}|;:,.<>?';
 
-  var letrasMayusculas = null
+  var letrasMayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  var caracteresDisponibles = null;
+  var caracteresDisponibles = letras;
 
-  if () {
-      
+  if (incluirEspeciales) {
+      caracteresDisponibles += especiales;
   }
 
-  if () {
-      
+  if (incluirNumeros) {
+      caracteresDisponibles += numeros;
   }
 
-  if () {
-      
-
+  if (incluirMayusculas) {
+      caracteresDisponibles += letrasMayusculas;
   }
 
-  var contrasena = null;
+  var contrasena = '';
 
-  if () {
-
+  if (checkLongitud(longitud)) {
+    for (let i = 0; i < longitud; i++) {
+      let valorAleatorio = Math.random();
+      let indice = valorAleatorio * caracteresDisponibles.length;
+      let indiceEntero = Math.floor(indice);
+      let caracter = caracteresDisponibles[indiceEntero];
+      contrasena += caracter;
+    }
   }
 
-  return null;
+  return "Contraseña generada: " + contrasena;
 }
 
 // <------- NO TOCAR -------->
@@ -42,3 +51,15 @@ module.exports = {
   checkLongitud,
   generarContrasena,
 };
+
+
+// Ejemplos de prueba
+console.log(generarContrasena(8, false, false, false)); 
+console.log(generarContrasena(10, true, false, false)); 
+console.log(generarContrasena(12, false, true, false)); 
+console.log(generarContrasena(15, false, false, true)); 
+console.log(generarContrasena(20, true, true, true));
+console.log(checkLongitud(3));
+console.log(checkLongitud(5));
+console.log(checkLongitud(2));
+console.log(checkLongitud(0));
