f7bd6555e73c314d8caea891e08fa25187efa47b
prodriguez25 <pablo25g.apr@gmail.com>
2025-09-01 00:07:20 -0300

    Homework FSM0L7 Arrays Monitoreo de actividades sospechosas

--- 04. JS Arrays/monitoreoActividad.js
+++ 04. JS Arrays/monitoreoActividad.js
@@ -1,9 +1,16 @@
 // <------- Arreglo de actividades sospechozas -----> modificar el valor de ser necesario
-var actividadesSospechozas = null
+var actividadesSospechozas = [];
 
 function agregarActividad(descripcion, nivelRiesgo){
     /* TU CODIGO */
-    
+    if (!descripcion || !nivelRiesgo) {
+        return "Descripción o nivel de riesgo no valido";
+     }
+    if (nivelRiesgo != "bajo" && nivelRiesgo != "medio" && nivelRiesgo != "alto"){
+        return "Nivel de riesgo no valido, el nivel debe ser : bajo, medio, o alto";
+    }
+    actividadesSospechozas.push("Descripción: " + descripcion + ", riesgo - " + nivelRiesgo);
+    return "Actividad: 'descripción' con Nivel de riego: 'nivelRiesgo' fue agregada con exito"
 }
 
 function eliminarActividad(indice){
